9d5af8b5fc850a4136f5f2d511793c349110521e
unBro2786 <seccion891@gmail.com>
2025-04-09 16:07:54 -0600

    Boton para limpiar tu nombre agregado

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
+let submitButton = document.getElementById("submitButton");
+let limpiarButton = document.getElementById("limpiarButton");
+
 /* La función almacenara el nombre en el localstorage */
 function submitButtonEvent() {
     let inputNombre = document.getElementById("nombre").value;
     localStorage.setItem("nombre", inputNombre);
 }
 
-let submitButton = document.getElementById("submitButton");
+function limpiarButtonEvent() {
+    localStorage.clear("nombre");
+}
+
 submitButton.addEventListener("click", submitButtonEvent);
+limpiarButton.addEventListener("click", limpiarButtonEvent);
